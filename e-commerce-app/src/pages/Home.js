3a29fb800363b48579c232521a489fcbae6d51b7
reyes.stevenalec <reyes.stevenalec@gmail.com>
2024-03-06 20:48:15 +0800

    removed react  Alert component

--- e-commerce-app/src/pages/Home.js
+++ e-commerce-app/src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Alert, Button } from "react-bootstrap";
+import { Container, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 import "./Home.css"; // Import your custom styles
